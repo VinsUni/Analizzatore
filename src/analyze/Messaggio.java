ed93e2d4856de12fc625c5a4a9d0ad6344a18489
Vins <v.plantone@studenti.uniba.it>
2017-12-20 16:29:58 +0100

    Apportate modifiche

--- src/analyze/Messaggio.java
+++ src/analyze/Messaggio.java
@@ -2,9 +2,10 @@ package analyze;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.util.Scanner;
 
-public class Messaggio {
+public class Messaggio implements Comparable<Object>, Serializable{
 
     private String messaggio;
     private int pericolosità;
@@ -22,5 +23,39 @@ public class Messaggio {
         } finally{
             in.close();
         }
+    }
+
+    public String getMessaggio() {
+        return messaggio;
+    }
+
+    public void setMessaggio(String messaggio) {
+        this.messaggio = messaggio;
+    }
+
+    public int getPericolosità() {
+        return pericolosità;
+    }
+
+    public void setPericolosità(int pericolosità) {
+        this.pericolosità = pericolosità;
+    }
+
+    @Override
+    public String toString() {
+        return "Messaggio{" +
+                "messaggio='" + messaggio + '\'' +
+                ", pericolosità=" + pericolosità +
+                '}';
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        if(o != null || o instanceof Messaggio){
+            Messaggio m = (Messaggio) o;
+            return -(this.getPericolosità()-m.getPericolosità());
+        }
+        return -1;
     }
 }
--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -1,5 +1,9 @@
 package main;
 
+import analyze.Messaggio;
+
+import java.io.File;
+
 /**
  * @author Vincenzo Plantone
  * @matricola 639371
@@ -9,6 +13,10 @@ public class TestMain {
 
     public static void main(String[] args) {
 
-
+        final int MEX = 7;
+        Messaggio[] messages = new Messaggio[MEX];
+        for(int i = 0; i < MEX; i++){
+            messages[i] = new Messaggio(new File(args[i]));
+        }
     }
 }
